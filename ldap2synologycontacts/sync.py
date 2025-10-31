be959d6ac5c43ca8f9c59469e01d7e0077af14fb
Imdabossou <jamie.lfths@hotmail.com>
2025-10-31 10:54:46 +0000

    Verify HTTPS Certificate

--- ldap2synologycontacts/sync.py
+++ ldap2synologycontacts/sync.py
@@ -6,20 +6,23 @@ import requests
 from config import carddav_url, carddav_username, carddav_password, carddav_vcf_directory
 
 for file_name in os.listdir(carddav_vcf_directory):
-    if file_name.endswith('.vcf'):
-        file_path = os.path.join(carddav_vcf_directory, file_name)
-        with open(file_path, 'r') as f:
-            vcf_data = f.read()
+    if file_name.endswith('.vcf'):
+        file_path = os.path.join(carddav_vcf_directory, file_name)
+        with open(file_path, 'r') as f:
+            vcf_data = f.read()
 
-        filename = os.path.basename(file_path)
-        headers = {'Content-Type': 'text/vcard', 'If-None-Match': '*'}
+        filename = os.path.basename(file_path)
+        headers = {'Content-Type': 'text/vcard', 'If-None-Match': '*'}
 
-        # Construct the URL for uploading the vCard
-        url = f"{carddav_url}/{filename}"
+        # Construct the URL for uploading the vCard
+        url = f"{carddav_url}/{filename}"
 
-        response = requests.put(url, data=vcf_data, auth=(carddav_username, carddav_password), headers=headers, verify=False)
+        response = requests.put(url, data=vcf_data, auth=(carddav_username, carddav_password), headers=headers, verify='synology.pem')
 
-        if response.status_code in [200, 201]:
-            print(f"Successfully uploaded: {filename}")
-        else:
-            print(f"Failed to upload {filename}: {response.status_code} {response.text}")
+        # Print logs
+        if response.status_code == 201:
+            print(f"Successfully uploaded: {filename}")
+        if response.status_code == 412:
+            print(f"Contact already exists {filename}")
+    else:
+        print(f"Failed to upload {filename}: {response.status_code} {response.text}")
